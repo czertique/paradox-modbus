421c9dc2223fcef4b4c485f8180d68df05c36071
czertique <>
2019-04-28 08:49:45 +0200

    fix test queue watch config location

--- test/watch_queue.sh
+++ test/watch_queue.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-CONFIG="../config.json"
+CONFIG="../config_prt3.json"
 TOPIC="myhome/paradox/#"
 
 mosquitto_sub -d --insecure --cafile $(jq -r '.queue.tls.capath' ${CONFIG}) --cert $(jq -r '.queue.tls.cert' ${CONFIG}) --key $(jq -r '.queue.tls.key' ${CONFIG}) -t ${TOPIC}
